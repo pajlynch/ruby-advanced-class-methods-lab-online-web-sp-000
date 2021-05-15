3f1b2b49efcc98a86f059a975516d8e4984fcaa2
Patrick Lynch <pajlynch@gmail.com>
2021-05-15 16:27:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
 
   def self.new_from_filename(filename)
     split_filename = filename.chop.chop.chop.chop.split (" - ")
-    song = self.new
+    song = Song.new
     song.name = split_filename[1]
     song.artist_name = split_filename[0]    
     song
