d19f6cdd2557dd5cba3dbdcc7327e3fb4c7d8d6f
Patrick Lynch <pajlynch@gmail.com>
2021-05-15 15:35:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,8 +36,8 @@ class Song
   def self.find_or_create_by_name(name)
     if self.find_by_name(name)
       self.find_by_name(name)
-
+    else
+      self.create_by_name(name)
     end
-
   end
 end
