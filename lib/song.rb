1435329ef58ec46a87a624b40dea4aad2269318d
Patrick Lynch <pajlynch@gmail.com>
2021-05-15 14:40:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,26 +15,26 @@ class Song
     #@@all << song
     song
   end
-  
+
   def new_by_name(name)
-    song = self.new 
+    song = self.new
     song.name = name
     song
   end
-  
+
   def create_by_name(name)
-    song = self.new 
+    song = self.new
     song.name = name
     @@all << song
     song
   end
-  
+
   def find_by_name(name)
     @@all.find{|x| x.name == name}
   end
-  
+
   def find_or_create_by_name(name)
-      #if self.find_by_name(name) == nil 
+      #if self.find_by_name(name) == nil
         #self.create_by_name(name)
       #else
         #self.find_by_name(name)
@@ -42,20 +42,20 @@ class Song
       self.find_by_name(name) || self.create_by_name(name)
       #the above statement is saying do this(if it is true) or that (if the first thing is not true and the second thing is true)
   end
-  
+
   def alphabetical()
     #returns all the songs instances in ascending (a-z) alphabetical order.
     @@all.sort_by{|x| x.name}
   end
-  
-  
+
+
   def new_from_filename(name)
-    song = self.new 
+    song = self.new
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
     song
   end
-  
+
   def create_from_filename(name)
   #class method should not only parse the filename correctly but should also save the song
     song = self.new
@@ -64,7 +64,7 @@ class Song
     @@all << song
     song
   end
-  
+
   def destroy_all()
     #reset the state of the @@all class variable to an empty array thereby deleting all previous song instances.
     @@all.clear
