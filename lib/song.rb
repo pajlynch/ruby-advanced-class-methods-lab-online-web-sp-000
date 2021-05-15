537b8b569e6d30e60e599f5531ec1900f557b8a6
Patrick Lynch <pajlynch@gmail.com>
2021-05-15 15:34:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,10 +33,11 @@ class Song
     @@all.detect{|song| song.name == name}
   end
 
-  def find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
     if self.find_by_name(name)
       self.find_by_name(name)
-    else
-      self.create_by_name(name)
+      
+    end
+    
   end
 end
